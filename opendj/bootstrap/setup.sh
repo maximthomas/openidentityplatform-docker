c87922d5bdf4b7a623616aee025d4a9c5e290284
maximthomas <maxim.thomas@gmail.com>
2018-12-25 13:40:14 +0300

    change prefix

--- opendj/bootstrap/setup.sh
+++ opendj/bootstrap/setup.sh
@@ -7,11 +7,10 @@ echo "Setting up default OpenDJ instance"
 
 /opt/opendj/setup --cli -p $PORT --ldapsPort $LDAPS_PORT --enableStartTLS --generateSelfSignedCertificate \
   --baseDN $BASE_DN -h localhost --rootUserPassword "$ROOT_PASSWORD" \
-  --acceptLicense --no-prompt #--sampleData 1
+  --acceptLicense --no-prompt --sampleData 1
 
 
-if [ -d /opt/opendj/bootstrap/ldif ]; then
-
+if [ -d /opt/opendj/bootstrap/ldif/99-user.ldif ]; then
   echo "loading initial schema for openam"
   /opt/opendj/bin/ldapmodify -D "$ROOT_USER_DN" -h localhost -p $PORT -w $ROOT_PASSWORD -f /opt/opendj/bootstrap/ldif/99-user.ldif
 
